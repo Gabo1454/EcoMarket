a39b932547753b37a6debd342f98a718585b3ac1
Gabo1454 <ga.aguila@duocuc.cl>
2025-06-17 11:58:58 -0400

    Se arregla msvc venta que no iniciaba.

--- msvc.venta/src/main/java/com/msvc/venta/client/ClienteClient.java
+++ msvc.venta/src/main/java/com/msvc/venta/client/ClienteClient.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "cliente-client", url = "${cliente.service.url}")
-public class ClienteClient {
+public interface ClienteClient {
 
+    @GetMapping("/api/clientes/{id}")
+    Object obtenerClientePorId(@PathVariable Long id);
 
 }
